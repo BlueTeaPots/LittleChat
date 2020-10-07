6bf0613cae640393f0cc21203e90b89bee8d8edc
Declan Fraser Rixon <drixon@debian-t430s>
2020-10-08 01:19:29 +1100

    Moved to ncurses for graphics

--- parser.py
+++ parser.py
@@ -4,16 +4,16 @@ class colors:
 
 def parseBuffer(rawBuffer):
     messages = rawBuffer.split("\n")
-    parsedBuffer = ""
-    for message in messages:
-        parsedBuffer += parseLine(message)
-    return parsedBuffer
+    messages.pop() # remove the empty string from the split
+    for i in range(0, len(messages)):
+        messages[i] = parseLine(messages[i])
+    return messages
 
 def parseLine(rawLine):
     nameSplit = rawLine.split("!", 1)
     messageSplit = rawLine.split(" :", 1)
     if len(nameSplit) == 2 and len(messageSplit) == 2:
-        name = colors.NAME + nameSplit[0][1:] + colors.ENDC
+        name = nameSplit[0][1:]
         message = messageSplit[1]
-        return name + ": " + message + "\n"
+        return name + ": " + message
     return ""
