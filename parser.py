a6a0d5e59582a17942fe3807eab015012f118875
Declan Fraser Rixon <drixon@debian-t430s>
2020-10-08 21:37:52 +1100

    Added color and break
    Added color to usernames, though this should eventually be passed off to a config file. Additionally refactored the parsing pipeline so that it returns the username and message seperately as a tupple. The code for ncurses now needs to be sbbbbeperated into a seperate file to make maintaining easier.

--- parser.py
+++ parser.py
@@ -1,13 +1,7 @@
-class colors:
-    NAME = "\033[92m"
-    ENDC = "\033[0m"
-
 def parseBuffer(rawBuffer):
     messages = rawBuffer.split("\n")
     messages.pop() # remove the empty string from the split
-    for i in range(0, len(messages)):
-        messages[i] = parseLine(messages[i])
-    return messages
+    return [parseLine(msg) for msg in messages if msg != ""]
 
 def parseLine(rawLine):
     nameSplit = rawLine.split("!", 1)
@@ -15,5 +9,7 @@ def parseLine(rawLine):
     if len(nameSplit) == 2 and len(messageSplit) == 2:
         name = nameSplit[0][1:]
         message = messageSplit[1]
-        return name + ": " + message
-    return ""
+        returned = (name + ":", " " + message)
+        return returned
+    returned = ("", "")
+    return returned
